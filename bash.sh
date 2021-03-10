4410bb90c694a73c58aa5d3ce57b12ac5a3e4643
johanpurwadhika <76193965+johanpurwadhika@users.noreply.github.com>
2021-03-10 10:23:31 +0700

    add line 3

--- bash.sh
+++ bash.sh
@@ -2,3 +2,4 @@
 
 echo "file ini dibuat menggunakan github webhook" > myfile.txt
 echo "'$BUILD_NUMBER' + '$USER'" >> myfile.txt
+echo "hallo ini baris yang dihasilkan oleh github triger" >> myfile.txt
